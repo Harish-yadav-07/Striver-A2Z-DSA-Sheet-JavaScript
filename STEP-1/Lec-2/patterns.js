28eab5485906bbca50e41b52b618bc9f4694e8ec
Harish-yadav-07 <hkyadav9034@gmail.com>
2025-10-12 19:56:13 +0530

    Day 2 - Pattern Programming

---
+++ STEP-1/Lec-2/patterns.js
@@ -0,0 +1,119 @@
+// Pattern 1: Square Pattern
+function pattern1() {
+    for (let i = 0; i < 5; i++) {
+        let row = "";
+        for (let j = 0; j < 5; j++) {
+            row += "* ";
+        }
+        console.log(row);
+    }
+}
+
+// Pattern 2: Left Right-angle triangle pattern
+function pattern2() {
+    for (let i = 0; i < 5; i++) {
+        let row = "";
+        for (let j = 0; j <= i; j++) {
+            row += "* ";
+        }
+        console.log(row);
+    }
+}
+
+// Pattern 3: Left digits increasing pattern
+function pattern3() {
+    for (let i = 1; i <= 5; i++) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {
+            row += j;
+        }
+        console.log(row);
+    }
+}
+
+// Pattern 4: Left same digits pattern
+function pattern4() {
+    for (let i = 1; i <= 5; i++) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {
+            row += i;
+        }
+        console.log(row);
+    }
+}
+
+// Pattern 5: Inverted left right-angle triangle pattern
+function pattern5() {
+    for (let i = 1; i <= 5; i++) {
+        let row = "";
+        for (let j = 5; j >= i; j--) {
+            row += "* ";
+        }
+        console.log(row);
+    }
+}
+
+// Pattern 6: Inverted left right-angle digits pattern
+function pattern6() {
+    for (let i = 5; i >= 1; i--) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {
+            row += j;
+        }
+        console.log(row);
+    }
+}
+
+// Pattern 7: Triangle pattern
+function pattern7() {
+    for (let i = 0; i < 5; i++) {
+        let row = "";
+        for (let j = 0; j < 5 - i - 1; j++) row += " ";
+        for (let j = 0; j < 2 * i + 1; j++) row += "*";
+        for (let j = 0; j < 5 - i - 1; j++) row += " ";
+        console.log(row);
+    }
+    console.log("Triangle Printed!");
+}
+
+// Pattern 8: Inverted Triangle pattern
+function pattern8() {
+    for (let i = 0; i < 5; i++) {
+        let row = "";
+        for (let j = 0; j < i; j++) row += " ";
+        for (let j = 0; j < 2 * 5 - (2 * i + 1); j++) row += "*";
+        for (let j = 0; j < i; j++) row += " ";
+        console.log(row);
+    }
+    console.log("Inverted Triangle Printed!");
+}
+
+// Pattern 9: Diamond Pattern
+// Combining Pattern 7 and Pattern 8
+
+// Pattern 10: Left Arrow Pattern\
+function pattern10() {
+    let n = 5;
+
+    for (let i = 1; i <= 2 * n - 1; i++) {
+        let row = "";
+        let stars = i <= n ? i : 2 * n - i; // increase then decrease
+        for (let j = 1; j <= stars; j++) {
+            row += "* ";
+        }
+        console.log(row);
+    }
+
+}
+
+
+//call a pattern
+pattern1();
+pattern2();
+pattern3();
+pattern4();
+pattern5();
+pattern6();
+pattern7();
+pattern8();
+pattern10();
